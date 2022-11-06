99dba34344b0e429336834464943102ed770f2b2
tonyd2020 <tonyd2ipad@gmail.com>
2022-11-06 13:25:11 +1100

    initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+// const canvas = document.getElementById('canvas');
+// const width = canvas.width;
+// const height = canvas.height;
+// let ctx = canvas.getContext('2d');
+
+
+// function circle(x, y, radius, clr) {
+//         ctx.save();
+//         ctx.translate(x, y);
+//         ctx.beginPath();
+//         ctx.fillStyle = clr;
+//         ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+//         ctx.fill();
+//         ctx.restore();
+// }
+
+// let radius = height * 0.50; 
+// let x = width/2;
+// let y = height/2;
+// circle(x, y, radius, 'rgba(0,80,255, 1)');
+
+// radius = radius * .8;
+// circle(x, y+ radius/4, radius, 'rgba(0,120,240)');
+
+// radius = radius * .7;
+// circle(x, y + radius * .7, radius, 'rgb(0,180,240,0.8)');
+
+// radius = radius * .6;
+// circle(x, y + radius*1.8 , radius, 'rgba(255,255,10, 1)');
